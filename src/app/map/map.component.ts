8d7bffeeb317937ea800334bb0c2eb120cbe1a32
Hwanghyebin99 <201802176@o.cnu.ac.kr>
2021-02-05 16:19:13 +0900

    add Angular Project

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit, Input, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ViewEncapsulation } from '@angular/core';
-
 import OlMap from 'ol/Map';
-import { fromLonLat } from 'ol/proj';
-import { MapService } from './map.service';
-import { MapidService } from './mapid.service';
-
+import OlView from 'ol/View';
+import Stamen from 'ol/source/Stamen';
+import OlTileLayer from 'ol/layer/Tile';
+import Colorize from 'ol-ext/filter/Colorize';
+import OLCesium from 'ol-cesium';
+import olcs from '../../../node_modules/olcs/core';
+import * as Cesium from 'cesium';
+window['Cesium'] = Cesium;
+window['CESIUM_BASE_URL'] = '/assets/cesium/';
 /**
  * Map Component: load and display a map
  * @example
@@ -20,53 +24,37 @@ import { MapidService } from './mapid.service';
     '../../../node_modules/ol/ol.css',
     '../../../node_modules/ol-ext/dist/ol-ext.css'
   ],
-  providers: [MapidService]
+
 })
 
 export class MapComponent implements OnInit {
 
-  /** Map id
-   */
-  @Input() id: string;
-
-  /** Longitude of the map
-   */
-  @Input() lon: string;
-
-  /** Latitude of the map
-   */
-  @Input() lat: string;
-
-  /** Zoom of the map
-   */
-  @Input() zoom: string;
-
-  /**
-   * [ol.Map](http://openlayers.org/en/latest/apidoc/ol.Map.html) Openlayer map object
-   */
   map: OlMap;
 
   constructor(
-    private mapService: MapService,
-    private mapidService: MapidService,
-    private elementRef: ElementRef
   ) {}
 
   /**
    * Create map on Init
    */
   ngOnInit() {
-    // Register a new id in the Mapid Service
-    this.mapidService.setId(this.id);
-    // Create a new map
-    this.map = this.mapService.getMap(this.id);
-    // If id is not defined place the map in the component element itself
-    if (!this.id) {
-      this.id = 'map';
-      this.map.setTarget(this.elementRef.nativeElement);
-    }
-    // Center on attribute
-    this.map.getView().setCenter(fromLonLat([parseFloat(this.lon) || 0, parseFloat(this.lat) || 0]));
-    this.map.getView().setZoom(this.zoom);
+
+     // The map
+    this.map = new OlMap
+     ({	target: 'map',
+       view: new OlView
+       ({	zoom: 8,
+         center: [247044, 6549736.]
+       }),
+       layers: []
+     });
+    const filter = new Colorize({
+      operation: 'grayscale',
+    });
+    let osm = new OlTileLayer({  source: new Stamen({ layer: 'watercolor' }),
+    filter:filter });
+
+    osm.addFilter(filter);
+    this.map.addLayer(osm);
   }
 }
--- src/app/map/map.module.ts
+++ src/app/map/map.module.ts
@@ -3,18 +3,11 @@ import { NgModule } from '@angular/core';
 import { MapService } from './map.service';
 
 import { MapComponent } from './map.component';
-import { LayerComponent } from './layer/layer.component';
-import { ControlComponent } from './control/control.component';
-import { MousePositionComponent } from './control/mouse-position.component';
-import { InteractionComponent } from './interaction/interaction.component';
+
 
 @NgModule({
   declarations: [
-    MapComponent,
-    LayerComponent,
-    ControlComponent,
-    MousePositionComponent,
-    InteractionComponent
+    MapComponent
   ],
   imports: [],
   providers: [
@@ -22,10 +15,7 @@ import { InteractionComponent } from './interaction/interaction.component';
   ],
   exports: [
     MapComponent,
-    LayerComponent,
-    ControlComponent,
-    MousePositionComponent,
-    InteractionComponent
+
  ]
 })
 export class MapModule { }
